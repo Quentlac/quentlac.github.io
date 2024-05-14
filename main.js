23e1e7c325004761aca4dd92c8c4609ac7789356
quentin.lacombe <quentin.lacombe@etu.univ-grenoble-alpes.fr>
2024-05-14 13:26:33 +0200

    Update

--- main.js
+++ main.js
@@ -31,7 +31,105 @@ window.addEventListener('load', function() {
     });
 
 
+    // Gestion du bouton pour remonter en haut de la page
+    if(this.window.scrollY > 100) {
+        document.querySelector('.go-top').style.right = '20px';
+    }
+
+    this.window.addEventListener('scroll', function() {
+        if(this.window.scrollY > 100) {
+            document.querySelector('.go-top').style.right = '20px';
+        } else {
+            document.querySelector('.go-top').style.right = '-150%';
+        }
+    });
+
+    document.querySelector('.go-top').addEventListener('click', function() {
+        window.scrollTo(0, 0);
+    });
+
+    // On regarde si on doit aller sur un projet en particulier
+    const params = new URLSearchParams(window.location.search);
+    const projet = params.get('projet');
+
+    if(projet) {
+        this.setTimeout(() => {
+            goToProjet(projet);
+        }, 1000);
+    }
+
+    this.document.querySelectorAll('.dot').forEach(dot => {
+        dot.addEventListener('click', function() {
+            goToProjet(dot.getAttribute('data-project'));
+        });
+    });
+
+
+
+    let prevX = 0;
+    let prevY = 0;
+
+    let count = 0;
+
+    // Fonction permettant de détecter l'intention de l'utilisateur
+    /*this.document.addEventListener('mousemove', function(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+
+
+        // On récupère les coordonnées du bouton de retour en haut de la page
+        const goTop = document.querySelector('.go-top');
+        const goTopRect = goTop.getBoundingClientRect();
+
+        const goTopX = goTopRect.x;
+        const goTopY = goTopRect.y;
+
+        // On calcule la direction de la souris
+        const directionX = x - goTopX;
+        const directionY = y - goTopY;
+
+        if(directionX == 0) return;
+
+        const direction = directionY / directionX;
+
+        // Calcul de la direction de la souris
+        const mouseDirectionX = prevX - x;
+        const mouseDirectionY = prevY - y;
+
+        if(mouseDirectionX == 0) return;
+
+        const mouseDirection = mouseDirectionY / mouseDirectionX;
+
+
+        console.log(Math.abs(mouseDirection - direction));
+
+        if(prevX > 0 && prevY > 0 && x > window.innerWidth / 2 && Math.abs(mouseDirection - direction) < 0.5) {
+            count++;
+            if(count > 10) {
+                document.querySelector('.go-top').classList.add('dev');
+            }
+        }
+        else {
+            count = 0;
+        }
+
+
+        prevX = x;
+        prevY = y;
+    
+    });*/
+
 });
+
+function goToProjet (projet) {
+    if(projet) {
+        const projetElement = document.querySelector(`#${projet}`);
+        if(projetElement) {
+            projetElement.scrollIntoView();
+        }
+    }
+}
+    
 
 function closePopupImage () {
     document.querySelector('.popup-image').classList.remove('popup-fadeIn');
